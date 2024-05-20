b0cf0b72e16a67898b54fd8405548d9faac309d1
sonomastea <sbstea@usf.edu>
2024-05-20 18:07:09 -0400

    Add accelerate method to increase speed by 10 km/h

--- cc_3.js
+++ cc_3.js
@@ -6,13 +6,9 @@ class car{
     this.make= make;
     this.speed= speed;
     }
-}
 
-const carOne = new car(BMW,120 km/h)
-const carTwo = new car(Mercedes,95 km/h)
- console.log(carOne);
- console.log(carTwo);
 
  //Step 2
  //Implement an accelerate method in the car class that increases the car's speed and logs the new speed to the console
-function calcTip(car){this.speed(+10); return 'the $${make} accelerated to $${speed}'.}
+accelerate() {this.speed +=10; console.log('New Spees: $ {this.speed} km/h');}
+}
