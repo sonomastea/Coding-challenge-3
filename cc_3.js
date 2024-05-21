cf9324c9fab0f5f776eb1ce6209ff24aadcc9ddc
sonomastea <sbstea@usf.edu>
2024-05-21 14:22:29 -0400

    Test accelerate and brake methods on Car instances

--- cc_3.js
+++ cc_3.js
@@ -21,14 +21,23 @@ class Car {
         {this.speed} km/h `);
 
     }
-    //Step 4
-
-  
-
-
-
-
-
-
-
+    
 }
+//Step 4
+const car1 = new Car(`BMW`, 120);
+console.log (`Car 1: ${car1.make}), 
+starting speed: ${car1.speed} km/h `);
+car1.accelerate();
+car1.accelerate();
+car1.brake();
+car1.brake();
+
+const car2 = new Car(`Mercedes`,95);
+console.log (`Car 2 : ${car2.make},
+starting speed: ${car2.speed} km/h `);
+car2.accelerate();
+car2.brake();
+car2.brake();
+car2.accelerate();
+
+
